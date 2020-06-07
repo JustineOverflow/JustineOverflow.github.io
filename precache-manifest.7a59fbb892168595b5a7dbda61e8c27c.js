93926c252d2abcffd94cd3f762cb8196d671c503
Justine Fabre <justinefabre84@gmail.com>
2020-06-07 09:04:30 +0100

    fixed button links

---
+++ precache-manifest.7a59fbb892168595b5a7dbda61e8c27c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eea4283ed476344a01c4e180a17a705f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d3d0092c9f69d6c1077",
+    "url": "/static/css/main.f7310028.chunk.css"
+  },
+  {
+    "revision": "b5d38674111223f26ad7",
+    "url": "/static/js/2.2307a170.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2307a170.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d3d0092c9f69d6c1077",
+    "url": "/static/js/main.995889cb.chunk.js"
+  },
+  {
+    "revision": "79532a0fd89d9fc5e8c8",
+    "url": "/static/js/runtime-main.8f57da58.js"
+  }
+]);
